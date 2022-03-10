68c929cdffba444a027f56e7a7dcec8d9be36b85
Restrepo Gutierrez, Esteban <erestrepogutierrez@ea.com>
2022-03-10 12:13:07 +0000

    Unit tests for size measuring

---
+++ Backend/CoreCpp/TinEye/TinEyeLib.Tests/size_tests.cpp
@@ -0,0 +1,130 @@
+#include <gtest/gtest.h>
+#include "TinEye.h"
+
+//All passing tests should pass with default configuration 
+TEST(SizeTests, 720Pass) {
+	TinEye* tineye = new TinEye();
+	tineye->init("config.json");
+
+	//Open input image with openCV
+	Image img;
+	img.loadImage("resources/sizes/PassingSizeTest720.png");
+
+	bool passesSize = false;
+	//Check if image has text recognized by OCR
+	tineye->applyFocusMask(img);
+	std::vector<Textbox> textBoxes = tineye->getTextBoxes(img);
+	tineye->mergeTextBoxes(textBoxes);
+
+	passesSize = tineye->fontSizeCheck(img, textBoxes);
+
+	delete tineye;
+
+	ASSERT_TRUE(passesSize);
+}
+
+TEST(SizeTests, 1080Pass) {
+	TinEye* tineye = new TinEye();
+	tineye->init("config.json");
+
+	//Open input image with openCV
+	Image img;
+	img.loadImage("resources/sizes/PassingSizeTest1080.png");
+
+	bool passesSize = false;
+	//Check if image has text recognized by OCR
+	tineye->applyFocusMask(img);
+	std::vector<Textbox> textBoxes = tineye->getTextBoxes(img);
+	tineye->mergeTextBoxes(textBoxes);
+
+	passesSize = tineye->fontSizeCheck(img, textBoxes);
+
+	delete tineye;
+
+	ASSERT_TRUE(passesSize);
+}
+
+TEST(SizeTests, 4kPass) {
+	TinEye* tineye = new TinEye();
+	tineye->init("config.json");
+
+	//Open input image with openCV
+	Image img;
+	img.loadImage("resources/sizes/PassingSizeTest4k.png");
+
+	bool passesSize = false;
+	//Check if image has text recognized by OCR
+	tineye->applyFocusMask(img);
+	std::vector<Textbox> textBoxes = tineye->getTextBoxes(img);
+	tineye->mergeTextBoxes(textBoxes);
+
+	passesSize = tineye->fontSizeCheck(img, textBoxes);
+
+	delete tineye;
+
+	ASSERT_TRUE(passesSize);
+}
+
+//All failing tests should fail with default configuration
+TEST(SizeTests, 720Fail) {
+	TinEye* tineye = new TinEye();
+	tineye->init("config.json");
+
+	//Open input image with openCV
+	Image img;
+	img.loadImage("resources/sizes/NotPassingSizeTest720.png");
+
+	bool passesSize = false;
+	//Check if image has text recognized by OCR
+	tineye->applyFocusMask(img);
+	std::vector<Textbox> textBoxes = tineye->getTextBoxes(img);
+	tineye->mergeTextBoxes(textBoxes);
+
+	passesSize = tineye->fontSizeCheck(img, textBoxes);
+
+	delete tineye;
+
+	ASSERT_FALSE(passesSize);
+}
+
+TEST(SizeTests, 1080Fail) {
+	TinEye* tineye = new TinEye();
+	tineye->init("config.json");
+
+	//Open input image with openCV
+	Image img;
+	img.loadImage("resources/sizes/NotPassingSizeTest1080.png");
+
+	bool passesSize = false;
+	//Check if image has text recognized by OCR
+	tineye->applyFocusMask(img);
+	std::vector<Textbox> textBoxes = tineye->getTextBoxes(img);
+	tineye->mergeTextBoxes(textBoxes);
+
+	passesSize = tineye->fontSizeCheck(img, textBoxes);
+
+	delete tineye;
+
+	ASSERT_FALSE(passesSize);
+}
+
+TEST(SizeTests, 4kFail) {
+	TinEye* tineye = new TinEye();
+	tineye->init("config.json");
+
+	//Open input image with openCV
+	Image img;
+	img.loadImage("resources/sizes/NotPassingSizeTest4k.png");
+
+	bool passesSize = false;
+	//Check if image has text recognized by OCR
+	tineye->applyFocusMask(img);
+	std::vector<Textbox> textBoxes = tineye->getTextBoxes(img);
+	tineye->mergeTextBoxes(textBoxes);
+
+	passesSize = tineye->fontSizeCheck(img, textBoxes);
+
+	delete tineye;
+
+	ASSERT_FALSE(passesSize);
+}
--- Backend/CoreCpp/TinEye/TinEyeLib/src/Textbox.cpp
+++ Backend/CoreCpp/TinEye/TinEyeLib/src/Textbox.cpp
@@ -2,15 +2,17 @@
 
 Textbox::Textbox(const std::vector<cv::Point >& points, int padding) : parentImage(nullptr) {
 	//Height takes into account possible box skewing when calculating due to letters going down (eg p's)
-	cv::Point topLeft(std::min(points[0].x, points[1].x), std::min(points[1].y, points[2].y));
+	cv::Point topLeft(std::min(points[0].x, points[1].x), points[1].y);
+	//Top left height is directly points[1] because through our testing we've found EAST tends to consistently
+	//place points[2] a bit higher and that messes up measurement accuracy
 	cv::Point bottomRight(std::max(points[2].x, points[3].x), std::max(points[0].y, points[3].y));
 	int boxHeight = bottomRight.y - topLeft.y;
 	int boxWidth = bottomRight.x - topLeft.x;
 
-	textboxRect = cv::Rect(topLeft.x,topLeft.y, boxWidth, boxHeight);
+	textboxRect = cv::Rect(topLeft.x, topLeft.y, boxWidth, boxHeight);
 }
 
-Textbox::Textbox(cv::Rect rect):parentImage(nullptr) {
+Textbox::Textbox(cv::Rect rect) :parentImage(nullptr) {
 	textboxRect = rect;
 }
 
@@ -32,16 +34,16 @@ cv::Mat Textbox::getSurroundingLuminanceHistogram(int marginX, int marginY)
 	//Make sure the surrounding bounding box is inside the original matrix's bounds
 	cv::Mat mat = parentImage->getImageMatrix();
 	int
-		marginLeft = (textboxRect.x-marginX >= 0) ? marginX : textboxRect.x,
+		marginLeft = (textboxRect.x - marginX >= 0) ? marginX : textboxRect.x,
 		marginRight = (textboxRect.x + textboxRect.width + marginX < mat.cols) ? marginX : mat.cols - textboxRect.width - textboxRect.x,
 		marginTop = (textboxRect.y - marginY >= 0) ? marginY : textboxRect.y,
 		marginBottom = (textboxRect.y + textboxRect.height + marginY < mat.rows) ? marginY : mat.rows - textboxRect.height - textboxRect.y;
-	return parentImage->calculateLuminanceHistogram(cv::Rect(textboxRect.x-marginLeft,textboxRect.y - marginTop,
-		textboxRect.width + marginLeft + marginRight,textboxRect.height + marginTop + marginBottom),
+	return parentImage->calculateLuminanceHistogram(cv::Rect(textboxRect.x - marginLeft, textboxRect.y - marginTop,
+		textboxRect.width + marginLeft + marginRight, textboxRect.height + marginTop + marginBottom),
 		cv::Rect(marginLeft, marginRight, textboxRect.width, textboxRect.height));
 }
 
-std::pair<float,float> Textbox::OverlapAxisPercentage(const Textbox& a, const Textbox& b) {
+std::pair<float, float> Textbox::OverlapAxisPercentage(const Textbox& a, const Textbox& b) {
 	cv::Rect aRect = a.textboxRect, bRect = b.textboxRect;
 	cv::Rect overlap = aRect & bRect;
 	float xOverlap = static_cast<float>(overlap.width) / std::min(aRect.width, bRect.width);
