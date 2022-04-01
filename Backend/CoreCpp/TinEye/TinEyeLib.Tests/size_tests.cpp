6ffbfad1723ef17f2fb0ba80963a3859262708e9
Esteban <erestrepogutierrez@ea.com>
2022-04-01 11:07:04 +0200

    Separated tests

--- Backend/CoreCpp/TinEye/TinEyeLib.Tests/size_tests.cpp
+++ Backend/CoreCpp/TinEye/TinEyeLib.Tests/size_tests.cpp
@@ -33,28 +33,124 @@ namespace tin {
 	};
 
 	//All passing tests should pass with default configuration 
-	TEST_F(SizeTests, 720Pass) {
-		ASSERT_TRUE(passesSize("resources/sizes/PassingSizeTest720.png"));
+	TEST_F(SizeTests, 720SerifPass) {
+		ASSERT_TRUE(passesSize("resources/sizes/720SerifPass.png"));
 	}
 
-	TEST_F(SizeTests, 1080Pass) {
-		ASSERT_TRUE(passesSize("resources/sizes/PassingSizeTest1080.png"));
+	TEST_F(SizeTests, 720SansPass) {
+		ASSERT_TRUE(passesSize("resources/sizes/720SansPass.png"));
 	}
 
-	TEST_F(SizeTests, 4kPass) {
-		ASSERT_TRUE(passesSize("resources/sizes/PassingSizeTest4k.png"));
+	TEST_F(SizeTests, 720MonospacePass) {
+		ASSERT_TRUE(passesSize("resources/sizes/720MonospacePass.png"));
+	}
+
+	TEST_F(SizeTests, 720BoldPass) {
+		ASSERT_TRUE(passesSize("resources/sizes/720BoldPass.png"));
+	}
+
+	TEST_F(SizeTests, 720ThinPass) {
+		ASSERT_TRUE(passesSize("resources/sizes/720ThinPass.png"));
+	}
+
+	TEST_F(SizeTests, 1080SerifPass) {
+		ASSERT_TRUE(passesSize("resources/sizes/1080SerifPass.png"));
+	}
+
+	TEST_F(SizeTests, 1080SansPass) {
+		ASSERT_TRUE(passesSize("resources/sizes/1080SansPass.png"));
+	}
+
+	TEST_F(SizeTests, 1080MonospacePass) {
+		ASSERT_TRUE(passesSize("resources/sizes/1080MonospacePass.png"));
+	}
+
+	TEST_F(SizeTests, 1080BoldPass) {
+		ASSERT_TRUE(passesSize("resources/sizes/1080BoldPass.png"));
+	}
+
+	TEST_F(SizeTests, 1080ThinPass) {
+		ASSERT_TRUE(passesSize("resources/sizes/1080ThinPass.png"));
+	}
+
+	TEST_F(SizeTests, 4kSerifPass) {
+		ASSERT_TRUE(passesSize("resources/sizes/4kSerifPass.png"));
+	}
+
+	TEST_F(SizeTests, 4kSansPass) {
+		ASSERT_TRUE(passesSize("resources/sizes/4kSansPass.png"));
+	}
+
+	TEST_F(SizeTests, 4kMonospacePass) {
+		ASSERT_TRUE(passesSize("resources/sizes/4kMonospacePass.png"));
+	}
+
+	TEST_F(SizeTests, 4kBoldPass) {
+		ASSERT_TRUE(passesSize("resources/sizes/4kBoldPass.png"));
+	}
+
+	TEST_F(SizeTests, 4kThinPass) {
+		ASSERT_TRUE(passesSize("resources/sizes/4kThinPass.png"));
 	}
 
 	//All failing tests should fail with default configuration
-	TEST_F(SizeTests, 720Fail) {
-		ASSERT_FALSE(passesSize("resources/sizes/NotPassingSizeTest720.png"));
+	TEST_F(SizeTests, 720SerifFail) {
+		ASSERT_FALSE(passesSize("resources/sizes/720SerifFail.png"));
 	}
 
-	TEST_F(SizeTests, 1080Fail) {
-		ASSERT_FALSE(passesSize("resources/sizes/NotPassingSizeTest1080.png"));
+	TEST_F(SizeTests, 720SansFail) {
+		ASSERT_FALSE(passesSize("resources/sizes/720SansFail.png"));
 	}
 
-	TEST_F(SizeTests, 4kFail) {
-		ASSERT_FALSE(passesSize("resources/sizes/NotPassingSizeTest4k.png"));
+	TEST_F(SizeTests, 720MonospaceFail) {
+		ASSERT_FALSE(passesSize("resources/sizes/720MonospaceFail.png"));
+	}
+
+	TEST_F(SizeTests, 720BoldFail) {
+		ASSERT_FALSE(passesSize("resources/sizes/720BoldFail.png"));
+	}
+
+	TEST_F(SizeTests, 720ThinFail) {
+		ASSERT_FALSE(passesSize("resources/sizes/720ThinFail.png"));
+	}
+
+	TEST_F(SizeTests, 1080SerifFail) {
+		ASSERT_FALSE(passesSize("resources/sizes/1080SerifFail.png"));
+	}
+
+	TEST_F(SizeTests, 1080SansFail) {
+		ASSERT_FALSE(passesSize("resources/sizes/1080SansFail.png"));
+	}
+
+	TEST_F(SizeTests, 1080MonospaceFail) {
+		ASSERT_FALSE(passesSize("resources/sizes/1080MonospaceFail.png"));
+	}
+
+	TEST_F(SizeTests, 1080BoldFail) {
+		ASSERT_FALSE(passesSize("resources/sizes/1080BoldFail.png"));
+	}
+
+	TEST_F(SizeTests, 1080ThinFail) {
+		ASSERT_FALSE(passesSize("resources/sizes/1080ThinFail.png"));
+	}
+
+	TEST_F(SizeTests, 4kSerifFail) {
+		ASSERT_FALSE(passesSize("resources/sizes/4kSerifFail.png"));
+	}
+
+	TEST_F(SizeTests, 4kSansFail) {
+		ASSERT_FALSE(passesSize("resources/sizes/4kSansFail.png"));
+	}
+
+	TEST_F(SizeTests, 4kMonospaceFail) {
+		ASSERT_FALSE(passesSize("resources/sizes/4kMonospaceFail.png"));
+	}
+
+	TEST_F(SizeTests, 4kBoldFail) {
+		ASSERT_FALSE(passesSize("resources/sizes/4kBoldFail.png"));
+	}
+
+	TEST_F(SizeTests, 4kThinFail) {
+		ASSERT_FALSE(passesSize("resources/sizes/4kThinFail.png"));
 	}
 }
