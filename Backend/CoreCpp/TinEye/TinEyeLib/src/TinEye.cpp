4d1aa7597bc9584ffb8fc2ee69b47f6a3b379a7a
Esteban <erestrepogutierrez@ea.com>
2022-04-04 10:58:51 +0200

    Mask dilation

--- Backend/CoreCpp/TinEye/TinEyeLib/src/TinEye.cpp
+++ Backend/CoreCpp/TinEye/TinEyeLib/src/TinEye.cpp
@@ -229,14 +229,16 @@ namespace tin {
 		//OTSU threshold automatically calculates best fitting threshold values
 		cv::Mat unsignedLuminance;
 		luminanceRegion.convertTo(unsignedLuminance, CV_8UC1, 255);
-		//TODO convert to 8bit unsigned for otsu
 		cv::threshold(unsignedLuminance, maskA, 0, 255, cv::THRESH_OTSU | cv::THRESH_BINARY);
-		cv::bitwise_not(maskA, maskB);
+		//Dilate and then substract maskA to get the outline of the mask
+		cv::dilate(maskA, maskB, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(7,7)));
+		maskB -= maskA;
 
 #ifdef _DEBUG
 		if (config->getAppSettings()->saveLuminanceMasks()) {
 			image.saveOutputData(unsignedLuminance, "lum.png");
 			image.saveOutputData(maskA, "mask.png");
+			image.saveOutputData(maskB, "outlineMask.png");
 		}
 #endif // _DEBUG
 
