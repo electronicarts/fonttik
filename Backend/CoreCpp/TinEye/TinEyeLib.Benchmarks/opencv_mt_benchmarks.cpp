c27091f2f097048492e277b1e622f377ef7f0454
Adrian Alvarez Bernabe <aalvarezbernabe@ea.com>
2022-05-10 11:26:25 +0200

    Iterative text recognition benchmark

--- Backend/CoreCpp/TinEye/TinEyeLib.Benchmarks/opencv_mt_benchmarks.cpp
+++ Backend/CoreCpp/TinEye/TinEyeLib.Benchmarks/opencv_mt_benchmarks.cpp
@@ -78,6 +78,37 @@ static void BM_TextRecognition(benchmark::State& state) {
 	}
 }
 
-BENCHMARK(BM_TextRecognition)->Iterations(5)->Unit(benchmark::kSecond)->ThreadRange(1, 8);
+//BENCHMARK(BM_TextRecognition)->Iterations(5)->Unit(benchmark::kSecond)->ThreadRange(1, 8);
+
+static void BM_TextRecognition_Iter(benchmark::State& state) {
+	cv::Mat img;
+	cv::dnn::TextRecognitionModel textRecognition = cv::dnn::TextRecognitionModel("crnn_cs.onnx");
+	textRecognition.setDecodeType("CTC-greedy");
+	std::ifstream vocFile;
+	vocFile.open("alphabet_94.txt");
+	CV_Assert(vocFile.is_open());
+	std::string vocLine;
+	std::vector<std::string> vocabulary;
+	while (std::getline(vocFile, vocLine)) {
+		vocabulary.push_back(vocLine);
+	}
+	textRecognition.setVocabulary(vocabulary);
+
+	// Normalization parameters
+	auto mean = cv::Scalar(127.5, 127.5, 127.5);
+	// The input shape
+	std::pair<int, int> size = { 100,32 };
+	textRecognition.setInputParams(1.0 / 127.5, cv::Size(size.first, size.second), cv::Scalar(mean[0], mean[1], mean[2]));
+
+	img = cv::imread("resources/knockout/Knockout1080.bmp", cv::IMREAD_COLOR);
+	for (auto _ : state) {
+		for (int i = 0; i < state.range(0); i++) {
+			textRecognition.recognize(img);
+		}
+	}
+}
+
+BENCHMARK(BM_TextRecognition_Iter)->UseRealTime()->Range(8, 8*8*8)->Unit(benchmark::kSecond)->ThreadRange(1, 8);
+
 
 BENCHMARK_MAIN();
